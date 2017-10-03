421914e7101abafccb190758bfe26f8b951e0d98
Michael Peck <mpeck417@gmail.com>
2017-10-03 10:09:41 -0500

    markup & css for doc pages

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
-<template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+<template lang="pug">
+  #app
+    .box
+      nav.nav-list
+        h1.nav-list--title Basics
+        ul.nav-list--items
+          li
+            a.nav-list--item(href="#") Getting Started
+          li
+            a.nav-list--item(href="#") Layout
+        h1.nav-list--title Components
+        ul.nav-list--items
+          li
+            a.nav-list--item(href="/alert") Alerts
+          li
+            a.nav-list--item(href="#") Buttons
+          li
+            a.nav-list--item(href="#") Tables
+    main.box
+      router-view
 </template>
 
 <script>
--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,3 +1,21 @@
 <template lang="pug">
-  alert(title='test' description='this is a sample alert')
+  div
+    h1 Alerts
+    p Use alerts to provide users with a highly visible message. The following are the 5 theming options.
+    .box.box-neutral.box-padded
+      alert(title="Headline for Default Alert")
+      alert(title="Headline for Success Alert" type="success")
+      alert(title="Headline for Warning Alert" type="warning")
+      alert(title="Headline for Error Alert" type="error")
+      alert(title="Headline for Info Alert" type="info")
+    h2.heading-3 Code
+    .box.box-neutral
+      pre
+        code
+          | &lt;alert title="Headline for Default Alert"&gt;&lt;/alert&gt;
+          | &lt;alert title="Headline for Success Alert" type="success"&gt;&lt;/alert&gt;
+          | &lt;alert title="Headline for Warning Alert" type="warning"&gt;&lt;/alert&gt;
+          | &lt;alert title="Headline for Error Alert" type="error"&gt;&lt;/alert&gt;
+          | &lt;alert title="Headline for Info Alert" type="info"&gt;&lt;/alert&gt;
+
 </template>
