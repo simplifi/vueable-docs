d84db8d30fc3c5e777b38715748c51f95e9a5186
Michael Peck <mpeck417@gmail.com>
2017-10-04 12:31:35 -0500

    alert page content, grid implementation

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,32 @@
 <template lang="pug">
   #app
-    .box
-      nav.nav-list
-        h1.nav-list--title Basics
-        ul.nav-list--items
-          li
-            router-link.nav-list--item(:to="{ name: 'index' }"
-                                       active-class="nav-list--item-is-selected"
-                                       :exact="true")
-              | Getting Started
-          li
-            a.nav-list--item(href="#") Layout
-        h1.nav-list--title Components
-        ul.nav-list--items
-          li
-            router-link.nav-list--item(:to="{ name: 'alert' }"
-                                       active-class="nav-list--item-is-selected"
-                                       :exact="true")
-              | Alerts
-          li
-            a.nav-list--item(href="#") Buttons
-          li
-            a.nav-list--item(href="#") Tables
-    main.box
-      router-view
+    .vu-l-row.vu-l-row-filled.vu-l-row-collapsed
+      .vu-l-col-4
+        .box.box-filled
+          nav.nav-list
+            h1.nav-list--title Basics
+            ul.nav-list--items
+              li
+                router-link.nav-list--item(:to="{ name: 'index' }"
+                                           active-class="nav-list--item-is-selected"
+                                           :exact="true")
+                  | Getting Started
+              li
+                a.nav-list--item(href="#") Layout
+            h1.nav-list--title Components
+            ul.nav-list--items
+              li
+                router-link.nav-list--item(:to="{ name: 'alert' }"
+                                           active-class="nav-list--item-is-selected"
+                                           :exact="true")
+                  | Alerts
+              li
+                a.nav-list--item(href="#") Buttons
+              li
+                a.nav-list--item(href="#") Tables
+      .vu-l-col-12
+        main.box.box-filled.box-adjacent
+          router-view
 </template>
 
 <script>
