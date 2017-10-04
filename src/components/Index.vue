e56350a53f8f96dd06645c21959e70d07f9f383f
Michael Peck <mpeck417@gmail.com>
2017-10-04 13:52:41 -0500

    remove unused css, nest grid one level deep

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,42 +1,30 @@
 <template lang="pug">
-  #app.vu-l-row.vu-l-row-filled.vu-l-row-collapsed
-    .vu-l-col-4
-      .box.box-filled
-        nav.nav-list
-          h1.nav-list--title Basics
-          ul.nav-list--items
-            li
-              router-link.nav-list--item(:to="{ name: 'home' }"
-                                         active-class="nav-list--item-is-selected"
-                                         exact)
-                | Getting Started
-            li
-              a.nav-list--item(href="#") Layout
-          h1.nav-list--title Components
-          ul.nav-list--items
-            li
-              router-link.nav-list--item(:to="{ name: 'alert' }"
-                                         active-class="nav-list--item-is-selected"
-                                         exact)
-                | Alerts
-            li
-              a.nav-list--item(href="#") Buttons
-            li
-              a.nav-list--item(href="#") Tables
-    .vu-l-col-12
-      main.box.box-filled.box-adjacent
-        router-view
-</template>
-
-<style lang="sass" scoped>
   #app
-    display: flex
-    min-height: 800px
-
-  #app > div.box
-    flex: 1 1 20%
-
-  #app > main.box
-    flex: 1 2 80%
-</style>
-
+    .vu-l-row.vu-l-row-filled.vu-l-row-collapsed
+      .vu-l-col-4
+        .box.box-filled
+          nav.nav-list
+            h1.nav-list--title Basics
+            ul.nav-list--items
+              li
+                router-link.nav-list--item(:to="{ name: 'home' }"
+                                           active-class="nav-list--item-is-selected"
+                                           exact)
+                  | Getting Started
+              li
+                a.nav-list--item(href="#") Layout
+            h1.nav-list--title Components
+            ul.nav-list--items
+              li
+                router-link.nav-list--item(:to="{ name: 'alert' }"
+                                           active-class="nav-list--item-is-selected"
+                                           exact)
+                  | Alerts
+              li
+                a.nav-list--item(href="#") Buttons
+              li
+                a.nav-list--item(href="#") Tables
+      .vu-l-col-12
+        main.box.box-filled.box-adjacent
+          router-view
+</template>
