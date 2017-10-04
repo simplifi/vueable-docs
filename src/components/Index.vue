d4e7c34eed1a2184eb5f56923ce8d84f42f95130
Brett Fincher <brettf@simpli.fi>
2017-10-04 12:55:18 -0500

    Nest routes and define flexbox layout

---
+++ src/components/Index.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+  #app
+    .box
+      nav.nav-list
+        h1.nav-list--title Basics
+        ul.nav-list--items
+          li
+            router-link.nav-list--item(:to="{ name: 'home' }"
+                                       active-class="nav-list--item-is-selected"
+                                       exact)
+              | Getting Started
+          li
+            a.nav-list--item(href="#") Layout
+        h1.nav-list--title Components
+        ul.nav-list--items
+          li
+            router-link.nav-list--item(:to="{ name: 'alert' }"
+                                       active-class="nav-list--item-is-selected"
+                                       exact)
+              | Alerts
+          li
+            a.nav-list--item(href="#") Buttons
+          li
+            a.nav-list--item(href="#") Tables
+    main.box
+      router-view
+</template>
+
+<style lang="sass" scoped>
+  #app
+    display: flex
+    min-height: 800px
+
+  #app > div.box
+    flex: 1 1 20%
+
+  #app > main.box
+    flex: 1 2 80%
+</style>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Index from '@/components/Index'
 import Home from '@/components/Home'
 import Alert from '@/components/Alert'
-
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [
-    { name: 'index', path: '/', component: Home },
-    { name: 'alert', path: '/alert', component: Alert },
+    {
+      name: 'index',
+      path: '/',
+      component: Index,
+      redirect: { name: 'home' },
+      children: [
+        { name: 'home', path: '', component: Home },
+        { name: 'alert', path: 'alert', component: Alert }
+      ]
+    },
     { path: '*', redirect: { name: 'index' } }
   ]
 })
